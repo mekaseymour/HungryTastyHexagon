7faad6bc819ca84674ea47b6a319f079cccfd137
Meka Seymour <mekaseymour@gmail.com>
2024-02-17 07:09:55 +0000

    Add spring to profile img animation

--- src/components/Foreground/components/Blocks/components/BioCard/BioCard.jsx
+++ src/components/Foreground/components/Blocks/components/BioCard/BioCard.jsx
@@ -10,39 +10,53 @@ import "./BioCard.css";
 const BioCard = ({ isOpen, onToggle }) => {
   return (
     <motion.div
+      data-isOpen={isOpen}
       data-block-name="bio"
-      layout
-      transition={{
-        opacity: { ease: "linear" },
-        layout: { duration: 0.3 },
-      }}
       animate={{
-        ...isOpen
+        ...(isOpen
           ? {
               backdropFilter: "blur(0px)",
               backgroundColor: "rgba(0, 0, 0, 0)",
               borderRadius: "32px",
               opacity: "1",
-              padding: "60px 32px 32px",
             }
           : {
               backdropFilter: "blur(30px)",
               backgroundColor: "rgba(0, 0, 0, 0.25)",
               borderRadius: "20px",
               opacity: "1",
-              padding: "12px",
-            },
-        transition: { duration: 0.5, ease: 'linear' }
+            }),
+        transition: { duration: 0.2 },
       }}
       className="container"
       onClick={isOpen ? () => {} : onToggle}
     >
-      <motion.div className="img-name-and-skills" animate={{ flexDirection: isOpen ? 'column-reverse' : 'row', transition: { duration: 0.5, ease: 'linear' } }}
+      <motion.div
+        className="img-name-and-skills"
+        animate={{
+          flexDirection: isOpen ? "column-reverse" : "row",
+          transition: { duration: 0.5 },
+        }}
       >
         <ProfileImg shouldBeFullScreen={isOpen} />
-        <motion.div className="name-and-skill" animate={{ gap: isOpen ? '12px' : '6px', transition: { duration: 0.5, ease: 'linear' } }}>
+        <motion.div
+          className="name-and-skill"
+          animate={{
+            gap: isOpen ? "12px" : "6px",
+            transition: { duration: 0.5 },
+          }}
+        >
           <h1 className="name">Elephant Brand</h1>
-          <motion.div className="skill" animate={{ backgroundColor: isOpen ? 'rgba(0, 0, 0, 0.4)' : 'rgba(230, 229, 229, 0.2)'}}>Creative direction</motion.div>
+          <motion.div
+            className="skill"
+            animate={{
+              backgroundColor: isOpen
+                ? "rgba(0, 0, 0, 0.4)"
+                : "rgba(230, 229, 229, 0.2)",
+            }}
+          >
+            Creative direction
+          </motion.div>
         </motion.div>
       </motion.div>
       <p className="bio">We create things people love</p>
--- src/components/Foreground/components/Blocks/components/BioCard/components/ProfileImg.jsx
+++ src/components/Foreground/components/Blocks/components/BioCard/components/ProfileImg.jsx
@@ -1,18 +1,33 @@
 import React from "react";
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
 import img from "../../../../../../../assets/logo.png";
 
 import "./ProfileImg.css";
 
-const ProfileImg = ({ shouldBeFullScreen}) => {
+const ProfileImg = ({ shouldBeFullScreen }) => {
   return (
-    <motion.div className="profile-img-container" animate={{ width: shouldBeFullScreen ? '100%' : '64px', height: shouldBeFullScreen ? 'auto' : '64px', borderRadius: shouldBeFullScreen ? '20px' : '8px', transition: { duration: 0.5 } }}>
+    <motion.div
+      className="profile-img-container"
+      animate={{
+        width: shouldBeFullScreen ? "100%" : "64px",
+        height: shouldBeFullScreen ? "auto" : "64px",
+        borderRadius: shouldBeFullScreen ? "20px" : "8px",
+        transition: shouldBeFullScreen
+          ? {
+              type: "spring",
+              stiffness: 100,
+              damping: 17,
+            }
+          : {
+              type: "spring",
+              stiffness: 100,
+              damping: 16,
+            },
+      }}
+    >
       <div className="profile-img-wrapper">
-        <img
-          src={img}
-          alt="profile-img"
-        />
+        <img src={img} alt="profile-img" />
       </div>
     </motion.div>
   );

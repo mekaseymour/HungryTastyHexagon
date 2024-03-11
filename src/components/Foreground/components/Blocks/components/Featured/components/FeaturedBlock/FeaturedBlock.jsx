dd8a3a159f0130d0c8fba43b08933ff5004d4167
Meka <mekaseymour@gmail.com>
2024-03-11 04:22:53 +0000

    Handle split featured section animations correctly

--- src/components/Foreground/components/Blocks/components/Featured/components/FeaturedBlock/FeaturedBlock.jsx
+++ src/components/Foreground/components/Blocks/components/Featured/components/FeaturedBlock/FeaturedBlock.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useContext } from "react";
 
-import Block from '../../../Block';
+import LayoutContext from "../../../../../../../../contexts/LayoutContext";
 
-import CONFIGS from '../../configs';
+import Block from "../../../Block";
+
+import CONFIGS, { FEATURE_BLOCK_IDS } from "../../configs";
 
 import "./FeaturedBlock.css";
 
 const FeaturedBlock = ({ id }) => {
+  const { engagedBlock } = useContext(LayoutContext);
+  const shouldAnimateOffscreenLeft =
+    engagedBlock === `featured-${FEATURE_BLOCK_IDS.right}` &&
+    id === FEATURE_BLOCK_IDS.left;
+  const shouldAnimateOffscreenRight =
+    engagedBlock === `featured-${FEATURE_BLOCK_IDS.left}` &&
+    id === FEATURE_BLOCK_IDS.right;
+  const getBlockLayout = () => {
+    if (shouldAnimateOffscreenLeft) return "offScreenLeft";
+    else if (shouldAnimateOffscreenRight) return "offScreenRight";
+    else return null;
+  };
+
   const { img, title } = CONFIGS[id];
 
   return (
-    <Block id={`featured-${id}`}>
+    <Block id={`featured-${id}`} layoutId={getBlockLayout()}>
       <div className={`featuredBlock ${id}`}>
         <img src={img} />
         <h3>{title}</h3>
